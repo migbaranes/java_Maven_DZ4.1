c401a3f73e61d3af972831c660e56ec6afaad0d8
migbaranes <migbaranes@gmail.com>
2023-11-28 21:54:00 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long sumBonus = service.calcBonus(5000,true);
+        System.out.println("Ваши бонусы составляют  " + sumBonus + "  баллов");
+    }
+}
